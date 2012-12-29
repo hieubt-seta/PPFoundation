00839b912cf24baf5eb34989984c1be0b207c35d
hieubt-seta <hieubt6142@setacinq.com.vn>
2012-12-29 16:28:37 +0700

    Add  FontsManager

---
+++ PPLibrary/PPFontsManager.h
@@ -0,0 +1,13 @@
+//
+//  PPFontsManager.h
+//  PPLibrary
+//
+//  Created by Hieu Bui on 12/29/12.
+//  Copyright (c) 2012 PIPU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPFontsManager : NSObject
+
+@end
---
+++ PPLibrary/PPFontsManager.m
@@ -0,0 +1,13 @@
+//
+//  PPFontsManager.m
+//  PPLibrary
+//
+//  Created by Hieu Bui on 12/29/12.
+//  Copyright (c) 2012 PIPU. All rights reserved.
+//
+
+#import "PPFontsManager.h"
+
+@implementation PPFontsManager
+
+@end
