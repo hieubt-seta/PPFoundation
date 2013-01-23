f90caa262d8818591062b43304cffc90b8e1fcd4
hieubt-seta <hieubt6142@setacinq.com.vn>
2013-01-23 17:32:31 +0700

    Add PPBaseRequest

---
+++ PPLibrary/PPBaseRequest.m
@@ -0,0 +1,13 @@
+//
+//  PPBaseRequest.m
+//  PPLibrary
+//
+//  Created by Hieu Bui on 12/29/12.
+//  Copyright (c) 2012 PIPU. All rights reserved.
+//
+
+#import "PPBaseRequest.h"
+
+@implementation PPBaseRequest
+
+@end
