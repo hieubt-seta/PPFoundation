c58a941661113b4b85499577cd42b23787b59c81
hieubt-seta <hieubt6142@setacinq.com.vn>
2013-03-08 15:34:45 +0700

    Remove JSONKit Add MFSideMenu

---
+++ PPLibrary/PPValidator.m
@@ -0,0 +1,133 @@
+//
+//  PPValidator.m
+//  winner21
+//
+//  Created by Hieu Bui on 3/8/13.
+//  Copyright (c) 2013 PIPU. All rights reserved.
+//
+
+#import "PPValidator.h"
+
+@implementation PPValidator
+
++ (BOOL)isNullOrNilObject:(id)object
+{
+    return object == [NSNull null] || object == nil;
+}
+
++ (BOOL)isValidObject:(id)object
+{
+    return ![self isNullOrNilObject:object];
+}
+
++ (BOOL)isValidEmail:(NSString*)email
+{
+    if (![PPValidator isValidObject:email]) {
+        return NO;
+    }
+    email = [email lowercaseString];
+    NSString *emailRegEx =
+    @"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
+    @"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
+    @"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"
+    @"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"
+    @"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"
+    @"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
+    @"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    
+    NSPredicate *regExPredicate =
+    [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegEx];
+    return [regExPredicate evaluateWithObject:email];
+
+}
+
++ (BOOL)isValidURL:(NSString *)candidate
+{
+    if (![PPValidator isValidObject:candidate]) {
+        return NO;
+    }
+    if (candidate.length == 0) {
+        return YES;
+    }
+    if ([candidate rangeOfString:@"http"].location == NSNotFound) {
+        candidate = [NSString stringWithFormat:@"http://%@", candidate];
+    }
+    NSString *urlRegEx =
+    @"^(http(?:s)?\\:\\/\\/[a-zA-Z0-9\\-]+(?:\\.[a-zA-Z0-9\\-]+)*\\.[a-zA-Z]{2,6}(?:\\/?|(?:\\/[\\w\\-]+)*)(?:\\/?|\\/\\w+\\.[a-zA-Z]{2,4}(?:\\?[\\w]+\\=[\\w\\-]+)?)?(?:\\&[\\w]+\\=[\\w\\-]+)*)$";
+    NSPredicate *urlTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", urlRegEx];
+    return [urlTest evaluateWithObject:candidate];
+    
+}
+
++ (NSString *)getStringFromInteger:(NSInteger)i
+{
+    return [NSString stringWithFormat:@"%d", i];
+}
+
++ (NSNumber *)getSafeIntegerFromObject:(id)obj
+{
+    if (obj == nil || [obj isKindOfClass:[NSNull class]]) {
+        return [NSNumber numberWithInt:INT_MIN];
+    }
+    if ([obj isKindOfClass:[NSNumber class]]) {
+        return obj;
+    }
+    if ([obj length] == 0) {
+        return [NSNumber numberWithInt:INT_MIN];
+    }
+    if ([obj isKindOfClass:[NSDictionary class]]) {
+        return [NSNumber numberWithInt:INT_MIN];
+    }
+    return [NSNumber numberWithInt:[obj intValue]];    
+}
+
++ (NSNumber *)getSafeFloatFromObject:(id)obj
+{
+    if (obj == nil || [obj isKindOfClass:[NSNull class]]) {
+        return [NSNumber numberWithInt:INT_MIN];
+    }
+    if ([obj isKindOfClass:[NSNumber class]]) {
+        return obj;
+    }
+    if ([obj length] == 0) {
+        return [NSNumber numberWithInt:INT_MIN];
+    }
+    if ([obj isKindOfClass:[NSDictionary class]]) {
+        return [NSNumber numberWithInt:INT_MIN];
+    }
+    return [NSNumber numberWithFloat:[obj floatValue]];
+    
+}
+
++ (NSNumber *)getSafeBoolFromObject:(id)obj
+{
+    if (obj == nil || [obj isKindOfClass:[NSNull class]]) {
+        return [NSNumber numberWithInt:INT_MIN];
+    }
+    if ([obj isKindOfClass:[NSNumber class]]) {
+        return obj;
+    }
+    if ([obj length] == 0) {
+        return [NSNumber numberWithInt:INT_MIN];
+    }
+    if ([obj isKindOfClass:[NSDictionary class]]) {
+        return [NSNumber numberWithInt:INT_MIN];
+    }
+    return [NSNumber numberWithBool:[obj boolValue]];
+}
+
++ (NSString *)getSafeStringFromObject:(id)obj
+{
+    if (obj == nil || [obj isKindOfClass:[NSNull class]]) {
+        return @"";
+    }
+    if ([obj isKindOfClass:[NSString class]]) {
+        return obj;
+    }
+    if ([obj isKindOfClass:[NSDictionary class]]) {
+        return @"";
+    }
+    return [obj stringValue];    
+}
+
+@end
